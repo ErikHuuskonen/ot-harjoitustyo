e1a2b58c51cd35c8fc8b9ef92420a43db969017c
Erik Huuskonen <eh.huuskonen@gmail.com>
2023-03-21 17:51:10 +0200

    tehtävään 7 asti

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,18 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_lataa_rahaa(self):
+        self.maksukortti.lataa_rahaa(1000)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 20.00 euroa")
+    
+    def test_ota_rahaa(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+    
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+    
+    def test_ota_rahaa_false(self):
+        self.maksukortti.ota_rahaa(1100)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
