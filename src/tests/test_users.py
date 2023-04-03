69be8e61a86a93c6fe130164a782f80846484692
Erik Huuskonen <eh.huuskonen@gmail.com>
2023-04-03 16:45:08 +0300

    vaihdettiin tkinter

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -1,19 +1,1 @@
 
-from PyQt5.QtWidgets import QPushButton
-from tietotila.all_windows import Users  
-
-
-def test_users_initial_state(qtbot):
-    users = Users()
-    qtbot.addWidget(users)
-    assert len(users.users_list) == 0
-    assert users.layout is not None
-    assert users.hbox_layout is not None
-
-def test_users_empty_button(qtbot):
-    users = Users()
-    qtbot.addWidget(users)
-    empty_button = users.findChild(QPushButton, "Uusi käyttäjä")
-    assert empty_button is not None
-
-
