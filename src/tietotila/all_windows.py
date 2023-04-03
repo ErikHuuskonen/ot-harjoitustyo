8197a7e36c701f04da6a54ac9cf1ea81d521a55a
Erik Standard <erikstandard@Eriks-MacBook-Pro.local>
2023-04-03 18:59:59 +0300

    siirrytty tk

--- src/tietotila/all_windows.py
+++ src/tietotila/all_windows.py
@@ -1,306 +1,32 @@
-import os
-from PyQt5.QtWidgets import QLabel, QVBoxLayout, QWidget, QApplication, QInputDialog, QPushButton, QHBoxLayout, QVBoxLayout
-from PyQt5.QtCore import Qt, QRect, QSize
-from PyQt5.QtGui import QPixmap, QColor, QPalette, QFont, QPen, QPainter
-from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5.QtGui import QPainter, QColor, QPen, QFont
-import sqlite3
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
 
+class LoadingScreen:
 
-
-class Start(QWidget):
     def __init__(self):
-        super().__init__()
-        self.logo_path = self.get_logo()
-        self.init_ui()
+        pass
+        
 
-    def init_ui(self):
-        self.layout = QVBoxLayout()
-        self.start_screen = self.create_window()
-        self.layout.addWidget(self.start_screen)
-        self.setLayout(self.layout)
-
-    def create_window(self):
-        pixmap = QPixmap(self.logo_path)
-        label = QLabel()
-        label.setAlignment(Qt.AlignCenter)
-        palette = label.palette()
-        palette.setColor(QPalette.Background, QColor("White"))
-        label.setAutoFillBackground(True)
-        label.setPalette(palette)
-        screen_geometry = QApplication.desktop().screenGeometry()
-        label.setGeometry(screen_geometry)
-        label.setPixmap(pixmap.scaled(label.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
-        return label
-
-    def get_logo(self):
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        parent_dir = os.path.dirname(os.path.dirname(script_dir))
-        resources_dir = os.path.join(parent_dir, 'resources')
-        resource_file = os.path.join(resources_dir, 'tietotila_startingscreen.jpg')
-        return resource_file
-
-class Users(QWidget):
+class MainWindow():
+    
     def __init__(self):
-        super().__init__()
-        self.init_ui()
-        self.users_list = []
-        self.empty()
+        self.window = tk.Tk()
+        self.window.attributes('-fullscreen', True)
+        self.title = self.window.title('Tietotila')
+        self.show_loading_screen()
 
-    def init_ui(self):
-        self.layout = QVBoxLayout(self)
-        self.hbox_layout = QHBoxLayout()
-        self.layout.addLayout(self.hbox_layout)
+    def show_loading_screen(self):
+        pass
 
-    def empty(self):
-        button = QPushButton("Uusi käyttäjä")
-        button.setObjectName("Uusi käyttäjä")
-        button.setFixedSize(300, 300)
-        button.setStyleSheet("""
-            QPushButton {
-                background-color: grey;
-                color: yellow;
-                font-size: 54px;
-                border-radius: 100px;
-            }
-            QPushButton:hover {
-                background-color: white;
-            }
-        """)
-        button.clicked.connect(self.empty_button_clicked)
-        #button.setStyleSheet("padding: 0; margin: 0;")
-        self.hbox_layout.setAlignment(Qt.AlignLeft)
-        self.add_button_to_layout(button)
+    def show_users(self):
+        users_frame = ttk.Frame(master=self.window)
+        users_frame.pack()
 
-    def empty_button_clicked(self):
-        self.create_new_button()
+        button_new_user = ttk.Button(master=users_frame, text='New User')
+        button_new_user.pack(pady=10)
 
-    def create_new_button(self):
-        text, ok = QInputDialog.getText(self, "Luo käyttäjä", "Käyttäjänimi:")
-        
-        if ok:
-            new_button = QPushButton(text)
-            new_button.setFixedSize(300, 300)
-            new_button.setStyleSheet("""
-            QPushButton {
-                background-color: grey;
-                color: yellow;
-                font-size: 54px;
-                border-radius: 100px;
-            }
-            QPushButton:hover {
-                background-color: white;
-            }
-        """)
-            self.users_list.append(str(new_button))
-            self.add_button_to_layout(new_button)
-            
-    
-    def add_button_to_layout(self, button):
-        self.hbox_layout.addWidget(button)
-        print(self.users_list)
-        
-
-class Documents(QWidget): 
-    def __init__(self):
-        super().__init__()
-        self.init_ui()
-        self.documents_list = []
-        self.empty()
-
-    def init_ui(self):
-        self.layout = QVBoxLayout(self)
-        self.hbox_layout = QHBoxLayout()
-        self.layout.addLayout(self.hbox_layout)
-
-    def empty(self):
-        button = QPushButton("Uusi tietotila")
-        button.setFixedSize(300, 300)
-        button.setStyleSheet("""
-            QPushButton {
-                background-color: grey;
-                color: yellow;
-                font-size: 54px;
-                border-radius: 100px;
-            }
-            QPushButton:hover {
-                background-color: white;
-            }
-        """)
-        button.clicked.connect(self.empty_button_clicked)
-        self.hbox_layout.setAlignment(Qt.AlignCenter)
-        self.add_button_to_layout(button)
-
-    def empty_button_clicked(self):
-        self.create_new_button()
-
-    def create_new_button(self):
-        text, ok = QInputDialog.getText(self, "Luo tietotila", "Tilan nimi:")
-        
-        if ok:
-            new_button = QPushButton(text)
-            new_button.setFixedSize(300, 300)
-            new_button.setStyleSheet("""
-            QPushButton {
-                background-color: grey;
-                color: yellow;
-                font-size: 54px;
-                border-radius: 100px;
-            }
-            QPushButton:hover {
-                background-color: white;
-            }
-        """)
-            self.documents_list.append(str(new_button))
-            self.add_button_to_layout(new_button)
-            
-    
-    def add_button_to_layout(self, button):
-        self.hbox_layout.addWidget(button)
-        print(self.documents_list)
-
-class Mindmap(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.master_node = {'name': "master", 'x': 500, 'y': 500, 'color': 'red', 'font_size': 20}
-        self.child_nodes = []
-        self.connections = []
-        self.scale_factor = 1.0
-        self.offset_x = 0
-        self.offset_y = 0
-        self.current_point = None
-        self.start_point = None
-        self.end_point = None
-        self.selected_node = None
-        self.drawing_line = False
-        self.panning = False
-
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.setRenderHint(QPainter.Antialiasing)
-        pen = QPen(Qt.black, 2, Qt.SolidLine)
-        painter.setPen(pen)
+        button_choose_user = ttk.Button(master=users_frame, text='Choose User')
+        button_choose_user.pack(pady=10)
 
         
-        for connection in self.connections:
-            start_node = connection['start_node']
-            end_node = connection['end_node']
-            painter.drawLine((start_node['x'] * self.scale_factor) + self.offset_x + start_node['width'] / 2,
-                            (start_node['y'] * self.scale_factor) + self.offset_y + start_node['height'] / 2,
-                            (end_node['x'] * self.scale_factor) + self.offset_x + end_node['width'] / 2,
-                            (end_node['y'] * self.scale_factor) + self.offset_y + end_node['height'] / 2)
-
-    
-        self.draw_node(painter, self.master_node)
-
-   
-        for child_node in self.child_nodes:
-            self.draw_node(painter, child_node)
-
-        if self.start_point is not None and self.drawing_line:
-            painter.drawLine(self.start_point, self.current_point)
-
-    def draw_node(self, painter, node):
-        font = QFont('SansSerif', node['font_size'])
-        painter.setFont(font)
-        painter.setBrush(QColor(node['color']))
-
-        x = (node['x'] * self.scale_factor) + self.offset_x
-        y = (node['y'] * self.scale_factor) + self.offset_y
-
-    
-        max_width = 30000
-
-       
-        wrapped_text = painter.fontMetrics().elidedText(node['name'], Qt.TextElideMode.ElideRight, max_width, Qt.TextWordWrap)
-        text_rect = painter.fontMetrics().boundingRect(QRect(x, y, max_width, 0), Qt.TextWordWrap | Qt.AlignLeft, wrapped_text)
-        text_width = text_rect.width()
-        text_height = text_rect.height()
-
-      
-        padding = 20
-        node['width'] = text_width + padding
-        node['height'] = text_height + padding
-
-       
-        painter.drawRect(x, y, node['width'], node['height'])
-
-        
-        painter.drawText(x + padding / 2, y + padding / 2, max_width, text_height, Qt.AlignLeft | Qt.TextWordWrap, wrapped_text)
-    
-    
-    
-    def mousePressEvent(self, event):
-        if event.button() == Qt.LeftButton:
-            self.start_point = event.pos()
-            self.selected_node = self.get_node_at_position(event.pos())
-            if self.selected_node:
-                self.drawing_line = True
-                self.current_point = event.pos()
-            else:
-                self.panning = True
-
-    def mouseDoubleClickEvent(self, event):
-        if event.button() == Qt.LeftButton:
-            clicked_node = self.get_node_at_position(event.pos())
-
-            if clicked_node is not None:
-                
-                input_dialog = QInputDialog(self)
-                input_dialog.setWindowTitle("Edit Node Text")
-                input_dialog.setLabelText("Enter new text:")
-                input_dialog.setTextValue(clicked_node['name'])
-                input_dialog.resize(QSize(1200, 2000))  
-                ok = input_dialog.exec_()
-                new_text = input_dialog.textValue()
-                if ok:
-                    clicked_node['name'] = new_text
-            
-            else:
-                
-                new_node = {'name': 'New Node',
-                            'x': (event.x() - self.offset_x) / self.scale_factor - 50,
-                            'y': (event.y() - self.offset_y) / self.scale_factor - 50,
-                            'color': 'white',
-                            'font_size': 16}
-                self.child_nodes.append(new_node)
-
-            self.update()
-
-    def mouseMoveEvent(self, event):
-        self.current_point = event.pos()
-        if self.selected_node is not None and not self.drawing_line and not self.panning:
-            self.selected_node['x'] = (event.x() - self.offset_x) / self.scale_factor - 50
-            self.selected_node['y'] = (event.y() - self.offset_y) / self.scale_factor - 50
-        elif self.panning:
-            self.offset_x += event.x() - self.start_point.x()
-            self.offset_y += event.y() - self.start_point.y()
-            self.start_point = event.pos()
-        self.update()
-
-    def mouseReleaseEvent(self, event):
-        if event.button() == Qt.LeftButton:
-            self.end_point = event.pos()
-            if self.drawing_line:
-                target_node = self.get_node_at_position(event.pos())
-                if target_node is not None and target_node != self.selected_node:
-                    self.connections.append({'start_node': self.selected_node, 'end_node': target_node})
-                self.drawing_line = False
-            self.panning = False
-            self.update()
-
-    def wheelEvent(self, event): 
-        angle = event.angleDelta().y()
-        if angle > 0:
-            self.scale_factor *= 1.5
-        else:
-            self.scale_factor *= 0.9
-        self.update()
-
-    def get_node_at_position(self, pos):
-        for node in [self.master_node] + self.child_nodes:
-            x, y, size = (node['x'] * self.scale_factor) + self.offset_x, (node['y'] * self.scale_factor) + self.offset_y, 100 * self.scale_factor
-            if x <= pos.x() <= x + size and y <= pos.y() <= y + size:
-                return node
-        return None
-
--- src/tietotila/main.py
+++ src/tietotila/main.py
@@ -1,36 +1,6 @@
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtCore import QTimer 
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
-from PyQt5.QtCore import Qt
+import tkinter as tk
+from tkinter import ttk
+from all_windows import MainWindow
 
-import sys
-from all_windows import Start, Users, Mindmap
-
-def main():
-    #aloitus screen
-    app = QApplication(sys.argv)
-
-    # Create main window and widgets
-    main_window = QMainWindow()
-    start = Start()
-    users = Users()
-    mindmap = Mindmap()
-
-    #näytä aloitus näyttö Start
-    main_window.setCentralWidget(start)
-    main_window.showFullScreen()
-
-    #viiden sekunnin jälkeen näytetään Users
-    QTimer.singleShot(5000, lambda: main_window.setCentralWidget(users))
-    
-    #users näyttää keskellä näyttöä neliskulmaisen harmaan napin jota painamalla voimme kirjoittaa uuden käyttäjän nimen ja tämä asetus astuu voimaan.
-    
-    #nyt voimme valita käyttäjän ja sen sisälle avautuu valikko jonka sisällä on neljä vaihtoehtoista tiedon arkistoa.
-
-    #astumme tiedon arkistoon ja siellä näemme avautuneen mind mapin jonka sisällä voimme astua taas uuteen mindmappiin jonka masted node on se otsikko jota klikkasimme.
-
-    sys.exit(app.exec_())
-
-if __name__=="__main__":
-    main()
+app = MainWindow()
+app.window.mainloop()
