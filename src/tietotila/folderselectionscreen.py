2578107194eb3ff8f6b626c2efc75739d7a89e5c
ErikHuuskonen <127846692+ErikHuuskonen@users.noreply.github.com>
2023-04-15 18:55:18 +0300

    pylint tyylittelyä

--- src/tietotila/folderselectionscreen.py
+++ src/tietotila/folderselectionscreen.py
@@ -1,61 +1,85 @@
+"""
+Tässä moduulissa on FolderSelectionScreen-luokka, joka luo ikkunan kansiovalintaa varten. 
+Luokka sisältää metodit uuden kansion luomiseen, kansion listaamiseen, piilottamiseen, käyttäjän asettamiseen ja kansion valitsemiseen. 
+Moduuli käyttää tkinter-kirjastoa ikkunan luomiseen ja simpledialog-kirjastoa kansion nimen kysymiseen.
+"""
 import tkinter as tk
 from tkinter import simpledialog
-
 class FolderSelectionScreen(tk.Frame):
-
+    """
+    Luokka kansion valintaikkunaa varten
+    """
     def __init__(self, window, on_mindmap_selected=None):
+        """
+        Alustaa FolderSelectionScreen-luokan annetulla ikkunalla ja on_mindmap_selected-kutsulla
+        """
         super().__init__(window)
         self.window = window
-        self.top = None  
+        self.top = None
         self.folders = []
+        self.current_user = None
         self.user = None
         self.on_mindmap_selected = on_mindmap_selected
-
         self.new_folder_button = tk.Button(self, text="New Folder", command=self.new_folder)
         self.new_folder_button.pack(side="left", padx=5, pady=5)
-
     def show(self, user):
+        """
+        Näyttää FolderSelectionScreenin annetulla käyttäjällä
+        """
         self.user = user
         self.pack(fill=tk.BOTH, expand=True)
         self.update_folder_list()
-
     def hide(self):
+        """
+        Piilottaa FolderSelectionScreenin
+        """
         self.pack_forget()
-
     def create_new_folder(self):
+        """
+        Luo uuden kansion annetulla nimellä ja päivittää kansion listan
+        """
         new_folder_name = simpledialog.askstring("Uusi tila", "Syötä tilan nimi:")
         if new_folder_name:
             self.create_folder(new_folder_name, self)
             self.update_folder_list()
-
     def update_folder_list(self):
-        print(f"Updating folder list for user: {self.user}")  
+        """
+        Päivittää kansion listan nykyisillä kansioilla ja tuhoaa aikaisemmat kansion painikkeet
+        """
+        print(f"Updating folder list for user: {self.user}")
         for widget in self.winfo_children():
             if isinstance(widget, tk.Button) and widget != self.new_folder_button:
                 widget.destroy()
-
         for folder in self.folders:
-            print(f"Adding folder button: {folder}")  
+            print(f"Adding folder button: {folder}")
             folder_button = tk.Button(self, text=folder, command=lambda f=folder: self.folder_button_pressed(f))
             folder_button.pack(side="left", padx=5, pady=5)
-
     def create_folder(self, folder_name, frame):
+        """
+        Luo uuden kansion annetulla nimellä annetussa kehyksessä
+        """
         new_button = tk.Button(frame, text=folder_name, command=lambda: self.folder_button_pressed(folder_name), height=2, width=10, background="green")
         new_button.pack(side="left", padx=5, pady=5)
-
         if folder_name not in self.folders:
             self.folders.append(folder_name)
-    
     def set_user(self, user):
+        """
+        set user
+        """
         self.current_user = user
-
-    def folder_button_pressed(self, folder_name): #mene tiedostoon mindmap näkymään. 
+    def folder_button_pressed(self, folder_name):
+        """
+        Käsittelee kansion painikkeen painalluksen ja kutsuu on_mindmap_selected-kutsua
+        """
         print(f"Folder selected: {folder_name}")
         if self.on_mindmap_selected:
             self.on_mindmap_selected()
-
     def new_folder(self):
+        """
+        Luo uuden kansion annetulla nimellä ja päivittää kansion listan
+        """
         folder_name = simpledialog.askstring("New Folder", "Enter folder name:")
         if folder_name and folder_name.strip():
             self.folders.append(folder_name.strip())
             self.update_folder_list()
+    
--- src/tietotila/loadingscreen.py
+++ src/tietotila/loadingscreen.py
@@ -1,43 +1,52 @@
+"""
+Moduulissa luodaan LoadingScreen-luokka, joka sisältää ikkunan latausnäytölle. 
+Luokka alustetaan antamalla sille window-parametrina tkinter-ikkuna, jonka avulla näyttö voidaan näyttää.
+"""
 import tkinter as tk
-import os
-import sys
 from PIL import Image, ImageTk
-
 class LoadingScreen:
+    """
+    LoadingScreen-luokka, joka sisältää ikkunan latausnäytölle. 
+    Luokka alustetaan antamalla sille window-parametrina tkinter-ikkuna, jonka avulla näyttö voidaan näyttää.
+    """
     def __init__(self, window):
+        """
+        Luokan rakentaja, joka luo uuden LoadingScreen-objektin.
+        Args:
+        window (tk.Tk): juuri-ikkuna, johon latausnäyttö tulee.
+        """
         self.window = window
         self.frame = tk.Frame(self.window)
-
-        #set image
         image = Image.open("/Users/erikstandard/Desktop/ot-harjoitustyo/resources/tietotila_startingscreen.gif")
-
-        # Convert the image to a format that can be displayed in a tkinter GUI
         photo = ImageTk.PhotoImage(image)
-
-        # Create a label to display the image
         self.image_label = tk.Label(self.frame, image=photo)
-        self.image_label.image = photo  # Save a reference to the photo to prevent garbage collection
+        self.image_label.image = photo
         self.image_label.pack(pady=10)
-    
-
         self.loading_label = tk.Label(self.frame, text="Loading Screen")
         self.loading_label.pack(pady=10)
         self.on_exit = None
         self.exit_button = tk.Button(self.frame, text="Continue", command= self.exit, fg="black")
         self.exit_button.place(x=100, y=100)
         self.exit_button.pack(pady=10)
-        
         self.window.after(5000, self.exit)
-
     def show(self):
+        """
+        Metodi, joka näyttää latausnäytön.
+        """
         self.frame.pack(fill=tk.BOTH, expand=True)
-
     def hide(self):
+        """
+        Metodi, joka piilottaa latausnäytön.
+        """
         self.frame.pack_forget()
-    
     def exit(self):
+        """
+        Metodi, joka sulkee latausnäytön.
+        """
         if self.on_exit:
             self.on_exit()
-    
     def get_path(self):
-        pass
+        """
+        Metodi, joka hakee aloitusnäytön kuvan.
+        """
+        
